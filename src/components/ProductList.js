d27f2f6c21792401f06f1d25950d10fefd151734
eChemmara <echemmara@gmail.com>
2025-01-08 12:32:17 +0800

    ProductList.js

---
+++ src/components/ProductList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { getProducts } from '../services/api';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await getProducts();
+      setProducts(response.data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
